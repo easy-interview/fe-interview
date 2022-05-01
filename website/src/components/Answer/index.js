3b214557a5c81c255c620baa5aada614461d3dce
wopen <nepow@foxmail.com>
2022-05-02 00:34:46 +0800

    Add anwer switcher

--- website/src/components/Answer/index.js
+++ website/src/components/Answer/index.js
@@ -6,14 +6,19 @@ export default function Answer({ children }) {
 
   const [show, setShow] = useState(false)
 
-  const showAnswer = useCallback(() => setShow(true), [])
+  const toggleShow = useCallback(() => setShow(!show), [show])
 
   return (
     <div>
-      <Admonition type="tip" title="答案">
+      <Admonition type="tip" title={
+        <div className='answer_header'>
+          <span>答案</span>
+          <div onClick={toggleShow} className={`answer_switch ${show ? 'answer_switch-active' : ''}`}></div>
+        </div>
+      }>
         <div className={`answer-content ${show ? 'answer-content-show' : ''}`}>
           {children}
-          <div className='answer-content-mask' onClick={showAnswer}></div>
+          <div className='answer-content-mask' onClick={toggleShow}></div>
         </div>
       </Admonition>
     </div>
